f779e60b9ca8855be3803ab7dbfead44fa3fa7f9
praneeth-2602 <praneeth.palugula@gmail.com>
2025-01-03 23:01:54 +0530

    handled the dependencies import

--- client/src/Pages/Testimonials.jsx
+++ client/src/Pages/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "../../node_modules/slick-carousel/slick/slick.css";
+import "../../node_modules/slick-carousel/slick/slick-theme.css";
 import "./Testimonials.css";
 import johnDoe from "../images/johndoe.png";
 import janesmith from "../images/janesmith.png";
